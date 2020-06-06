1114356f06972702a65b570af4c152c2977457f3
Kateryna Basova <katy.bas@mail.ru>
2020-06-06 22:52:55 +0300

    add comments and header

--- src/com/company/IGeometry.java
+++ src/com/company/IGeometry.java
@@ -1,6 +1,15 @@
 package com.company;
 
+/*
+    Interface IGeometry
+    Module 1. Task 2.
+
+    Copyright --- Kateryna Basova KNHUE
+*/
+
 public interface IGeometry {
+    // methods, which should be implemented by class
+    // which will implement the interface
     public double getVolume();
     public double getPerimeter();
 }
--- src/com/company/IPackaging.java
+++ src/com/company/IPackaging.java
@@ -1,6 +1,15 @@
 package com.company;
 
+/*
+    Interface IPackaging
+    Module 1. Task 2.
+
+    Copyright --- Kateryna Basova KNHUE
+*/
+
 public interface IPackaging {
+    // methods, which should be implemented by class
+    // which will implement the interface
     String toString();
     String toJSON();
     String toXML();
